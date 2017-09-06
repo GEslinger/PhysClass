f6420e9d6976bb1212055afae3ccdf612b931d87
Griffin Eslinger <griffin.eslinger01@gmail.com>
2017-09-05 22:51:29 -0400

    Final version

--- SHM/SHMMethods.cpp
+++ SHM/SHMMethods.cpp
@@ -3,16 +3,66 @@
 #include <iostream>
 #include <functional>
 #include <iomanip>
+#include <string>
+#include <cmath>
 #include "Tools.hpp"
 using namespace std;
 
-const double om = 0.5;
+const double x_0 = inputDouble(0, "Initial Position");		// Initial position
+const double v_0 = inputDouble(10, "Initial Velocity");		// Initial velocity
+const double dt = inputDouble(0.005, "Time Step");			// Time step
+const double k = inputDouble(25, "Spring Constant");		// Spring Constant
+const double m = inputDouble(1, "Mass");					// Mass
+const double om = sqrt(k/m);								// Omega
+const double SIM_TIME = inputDouble(50, "Simulation Time");	// Time to simulate
 
-function<double(double,double)> springStep = [](double x, double v){ return -om*om*x; };
+
+function<double(double,double)> springStep = [](double x, double v){ return -om*om*x; };	// The lambda function used as the differential equation
+
+
+Path1D applyMethod(Euler1D* obj){	// Function to apply euler method to a pointer of Euler1D, allowing for polymorphism
+	Path1D out;						// Define an empty path to put the results in and output
+
+	for(double t = 0; t < SIM_TIME; t += dt){	// Iterate from 0 to SIM_TIME with the defined time step
+		out.x.push_back(obj->getX());	// Add the current position to the path
+		out.t.push_back(t);				// Add the current time to the path
+		obj->update(dt);				// Apply the update method of obj (either Euler's or Euler-Cromer)
+	}
+
+	return out;	// Return the path variable
+}
+
+
+void plotPath(Path1D p, double amp, string title, string fname){	// Used to plot a path, specialized for this program...
+	// Accepts input for the path, maximum amplitude (so graphs have the same scale and aren't terrible), the title of the method, and the file name to output
+	Gnuplot gp;	// Instance of gnuplot terminal stream
+
+	gp << setprecision(3);
+	gp << "set xrange [0:" << SIM_TIME << "]\n";
+	gp << "set yrange [" << -amp*1.1 << ":" << amp*1.1 << "]\n";
+	gp << "set format y \"%.1f\"\n";
+	gp << "set term png size 720,480 font \"FreeSerif,12\"\n";
+	gp << "set xlabel \"t (s)\"\n";
+	gp << "set ylabel \"x (m)\"\n";
+	gp << "set title \"Position Vs. Time in Simple Harmonic Motion\\nUsing " << title << "\n"; 
+	gp << "set output \"" << fname << "\"\n";
+	gp << "plot '-' with dots lc rgb \"black\" notitle\n";
+	gp.send1d(boost::make_tuple(p.t,p.x));	// Separate the path into components and plot it!
+}
+
 
 int main(){
-	int b = 20;
-	inspect(b);
+	Euler1D* badSpring = new Euler1D(x_0, v_0, springStep);			// Make a pointer to a Euler1D object
+	Euler1D* goodSpring = new EulerCromer1D(x_0, v_0, springStep);	// And one to a EulerCromer1D object
 
-	return 0;
+	Path1D badPath = applyMethod(badSpring);	// Apply the method for both objects
+	Path1D goodPath = applyMethod(goodSpring);
+
+	double bigAmp = getMaxVal(badPath.x);	// Amplitude of the oscillation (max value of pos)
+	plotPath(badPath, bigAmp, "Euler's Method", "BadMethod.png");			// Plot them accordingly!
+	plotPath(goodPath, bigAmp, "The Euler-Cromer Method", "GoodMethod.png");
+
+	delete badSpring;	// Garbage collection to free up allocated memory
+	delete goodSpring;
+	return 0;			// All done!
 }
--- SHM/Tools.hpp
+++ SHM/Tools.hpp
@@ -1,17 +1,25 @@
-#ifndef INCLUDED_TOOLS
+#ifndef INCLUDED_TOOLS		// Protection against double-inclusion, make sure this file hasn't been included by
+#define INCLUDED_TOOLS 1	// checking against a flag, then set that flag
 #include <functional>
 #include <iostream>
-#define INCLUDED_TOOLS 1
-#define inspect(x) std::cout << #x << " is " << x << " in file " << __FILE__ << " at line " << __LINE__ << std::endl
+#include <vector>
+#include <string>
+#define inspect(x) std::cerr << #x << " is " << x << " in file " << __FILE__ << " at line " << __LINE__ << std::endl
 
 
-class Euler1D{						// Euler1D definition
+struct Path1D{				// A custom data type that makes passing around the position and time values easier
+	std::vector<double> x;	// Position
+	std::vector<double> t;	// Time
+};
+
+
+class Euler1D{	// Euler1D definition
 public:
 	Euler1D(double x_0, double v_0, std::function<double(double,double)>& func);// Constructor
-	virtual void update(double dt);// Euler step and updating x (virtual signifies the method can be overridden by EulerCromer1D)
-	double getX() const;	// Accessor methods
+	virtual void update(double dt);	// Euler step and updating x (virtual signifies the method can be overridden by EulerCromer1D)
+	double getX() const;			// Accessor methods
 	double getV() const;
-protected:
+protected:		// Protected instead of private allows the child class to make modifications (override mutator functions)
 	double x;	// Instance variables
 	double v;
 	std::function<double(double,double)> f; // Euler step
@@ -19,11 +27,12 @@ protected:
 
 Euler1D::Euler1D(double x_0, double v_0, std::function<double(double,double)>& func){
 	x = x_0;	// Constructor initializes position, velocity, and euler step
+	inspect(x);
 	v = v_0;
 	f = func;
 }
 
-void Euler1D::update(double dt){	// Update using euler method, dt argument allows for dynamic timestep
+void Euler1D::update(double dt){// Update using euler method, dt argument allows for dynamic timestep
 	double eulerStep = f(x,v);	// Calculate the step before updating anything
 	x += v*dt;					// Update x
 	v += eulerStep*dt;			// Update v
@@ -33,16 +42,54 @@ double Euler1D::getX() const { return x; }	// Accessor methods for x and v
 double Euler1D::getV() const { return v; }
 
 
-class EulerCromer1D : public Euler1D{	// EulerCromer1D inherits from Euler1D since they are almost identical
+class EulerCromer1D : public Euler1D{	// EulerCromer1D inherits from Euler1D with regular permissions since they are almost identical
 public:
+	EulerCromer1D(double x_0, double v_0, std::function<double(double,double)> func);
 	void update(double dt) override;	// Override the update function from Euler1D to conserve energy
-	using Euler1D::update;
 };
+
+EulerCromer1D::EulerCromer1D(double x_0, double v_0, std::function<double(double,double)> func)	// Constructor for EulerCromer1D
+: Euler1D(x_0, v_0, func){}																		// All it needs to do is call the super constructor
 
 void EulerCromer1D::update(double dt){
 	double eulerStep = f(x,v);	// Calculate the step before updating anything
 	v += eulerStep*dt;			// Update v
 	x += v*dt;					// Update x using v_(n+1) to create a backwards derivative and ensure energy conservation
+}
+
+
+double getMaxVal(std::vector<double> a){// My function to get the largest value in a vector
+	double maxVal = a[0];				// Initialize the maximum with a best guess (the first element of a)
+	for(int i = 0; i < a.size(); i++){	// Iterate over a
+		if(a[i] > maxVal) maxVal = a[i];// If a[i] is greater than the maximum, consider it the new maximum
+	}
+	return maxVal;	// Return the max
+}
+
+
+double inputDouble(double def, std::string title){
+	std::string input;
+	double output;
+	bool valid = false;
+
+	std::cout << "Enter a valid double for " << title;
+	std::cout << " (default is " << def << "): ";
+
+	while(!valid){
+		try{
+			std::getline(std::cin, input);
+			if(input.empty()) return def;
+
+			output = std::stod(input);
+			if(output != output) throw "Not a Number!";
+			valid = true;
+		} catch(...) {
+			std::cout << "Invalid input, re-enter: ";
+		}
+	}
+
+	// std::cout << std::endl << output << std::endl;
+	return output;
 }
 
 
